f18d77f7df70b6a1662c424946374e44bf2ab4ee
rafale77 <anhmantran@yahoo.com>
2020-11-21 14:10:32 -0800

    Use luup inet

--- L_Amcrest.lua
+++ L_Amcrest.lua
@@ -5,24 +5,9 @@ _DESCRIPTION = "Amcrest plugin"
 _AUTHOR = "rafale77"
 --]==]
 
-local is_LuaJIT = ({false, [1] = true})[1]
 local CAM_SID = "urn:micasaverde-com:serviceId:Camera1"
 local HAD_SID = "urn:micasaverde-com:serviceId:HaDevice1"
 local SES_SID = "urn:micasaverde-com:serviceId:SecuritySensor1"
-if is_LuaJIT then
-  http = require("luajit-request")
-else
-  http = require("http-digest")
-end
-
-local function digest(url)
-  local response = http.request(url, {
-    auth_type = "digest",
-	  username = luup.devices[lul_device].user,
-	  password = luup.devices[lul_device].pass
-   })
-  return response
-end
 
 function getSetVariable(serviceId, name, deviceId, default)
   local curValue = luup.variable_get(serviceId, name, deviceId)
@@ -34,27 +19,26 @@ function getSetVariable(serviceId, name, deviceId, default)
 end
 
 function cmdsend(code, func, arg)
-  local url = "http://" ..luup.devices[lul_device].ip.. "/cgi-bin/" ..func.. ".cgi?action=start&channel=0&code=".. code .. "&arg1=0&arg2=" .. arg .. "&arg3=0"
-  digest(url)
-  --luup.inet.wget(url)
+  local url = ("http://" ..luup.devices[lul_device].user.. ":" ..luup.devices[lul_device].pass.. "@" ..luup.devices[lul_device].ip.. "/cgi-bin/" ..func.. ".cgi?action=start&channel=0&code=".. code .. "&arg1=0&arg2=" .. arg .. "&arg3=0")
+  luup.inet.wget(url)
 end
 
 function snapcmd()
-  url = "http://" ..luup.devices[lul_device].ip.. "/cgi-bin/snapshot.cgi"
-  digest(url)
+  url = ("http://" ..luup.devices[lul_device].user.. ":" ..luup.devices[lul_device].pass.."@" ..luup.devices[lul_device].ip.. "/cgi-bin/snapshot.cgi")
+  luup.inet.wget(url)
 end
 
 function stopPanTiltZoom()
-  local url = "http://" ..luup.devices[lul_device].ip.. "/cgi-bin/ptz.cgi?action=stop&channel=0&code=Up&arg1=0&arg2=0&arg3=0"
-  digest(url)
+  local url = ("http://" ..luup.devices[lul_device].user.. ":" ..luup.devices[lul_device].pass.. "@" ..luup.devices[lul_device].ip.. "/cgi-bin/ptz.cgi?action=stop&channel=0&code=Up&arg1=0&arg2=0&arg3=0")
+  luup.inet.wget(url)
 end
 
 --Enable motion and audio detect (true/false)
 function DetectionEnable(action)
-  url = "http://" ..luup.devices[lul_device].ip.. "/cgi-bin/configManager.cgi?action=setConfig&amp;MotionDetect[0].Enable=" .. action
-  digest(url)
-  url = "http://" ..luup.devices[lul_device].ip.. "/cgi-bin/configManager.cgi?action=setConfig&amp;AudioDetect[0].AnomalyDetect.Enable=" .. action .. "&amp;AudioDetect[0].MutationDetect=" .. action
-  digest(url)
+  url = ("http://" ..luup.devices[lul_device].user.. ":" ..luup.devices[lul_device].pass.. "@" ..luup.devices[lul_device].ip.. "/cgi-bin/configManager.cgi?action=setConfig&amp;MotionDetect[0].Enable=" .. action)
+  luup.inet.wget(url)
+  url = ("http://" ..luup.devices[lul_device].user.. ":" ..luup.devices[lul_device].pass.. "@" ..luup.devices[lul_device].ip.. "/cgi-bin/configManager.cgi?action=setConfig&amp;AudioDetect[0].AnomalyDetect.Enable=" .. action .. "&amp;AudioDetect[0].MutationDetect=" .. action)
+  luup.inet.wget(url)
 end
 
 function init(lul_device)
